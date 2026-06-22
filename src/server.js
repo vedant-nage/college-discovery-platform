d8a6a3ffd8deb97696c93b7d81d1be40abcdf060
Vedant Nage <vedantnage2@gmail.com>
2026-06-22 19:35:07 +0530

    Fix render port

--- src/server.js
+++ src/server.js
@@ -7,12 +7,12 @@ import app from "./app.js";
 import connectDB from "./config/db.js";
 
 connectDB();
-
+const PORT = process.env.PORT || 5000;
 app.listen(
   process.env.PORT,
   () => {
     console.log(
-      `Server Running On ${process.env.PORT}`
+      `Server Running On ${PORT}`
     );
   }
 );
