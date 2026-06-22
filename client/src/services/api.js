874d23e359aa4678415d6403ba86c6a209d37fef
Vedant Nage <vedantnage2@gmail.com>
2026-06-22 20:05:37 +0530

    Update production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+ baseURL: "https://college-discovery-platform-udwz.onrender.com/api"
 });
 
 export default API;
